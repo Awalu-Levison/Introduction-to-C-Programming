abc0b32bcfe0938ba5bb3f32278486aae180342c
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-25 14:55:38 +0000

    Copying memory to destination variable

---
+++ pointers/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  include "main.h"
+ */**
+ * *_memcpy - A function that copies memory area
+ *  * @dest: Destination variable that receives bytes from src
+ *   * @src: The source of bytes to be used
+ *    * @n: Available number of bytes
+ *     * Return: Destination bytes
+ *      */
+ char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	        int y = 0;
+
+		        int v = n;
+
+			        for (; y < v; y++)
+					        {
+							                dest[y] = src[y]
+										                n--;
+									        }
+				        return (dest);
+}
