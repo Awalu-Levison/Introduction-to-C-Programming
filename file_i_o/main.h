03a74109a59a6b2e2d47c604c50956814d128b14
Awalu-Levison <levisonawalu251@gmail.com>
2023-12-03 22:13:12 +0200

    Message

--- file_i_o/main.h
+++ file_i_o/main.h
@@ -1,29 +1,15 @@
-#include "main.h"
-/**
- * append_text_to_file - A function to append text to a file
- * @filename: Pointer to the actual file
- * @text_content: The content of a file
- * Return: 1 on success or -1
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int fdp;
-	int n;
-	int i; /*rwr*/
+#ifndef MAIN_H
+#define MAIN_H
 
-	if (!filename)
-		return (-1);
-	fdp = open(filename, O_WRONLY | O_APPEND);
-	if (fdp == -1)
-		return (-1);
-	if (text_content)
-	{
-		for (n = 0; text_content[n]; n++)
-			;
-		i = write(fdp, text_content, n);
-		if (i == -1)
-			return (-1);
-	}
-	closse(fdp);
-	return (1);
-}
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
